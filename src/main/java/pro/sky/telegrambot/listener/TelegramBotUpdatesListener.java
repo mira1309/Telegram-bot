26c3357e57d7eaa4a329d735c3b651fe96c8c2f4
mira1309 <134642665+mira1309@users.noreply.github.com>
2024-02-05 01:00:26 +0300

    initial code 2

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -8,9 +8,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.entity.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -19,6 +25,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    public TelegramBotUpdatesListener(NotificationTaskRepository notificationTaskRepository){
+       this.notificationTaskRepository=notificationTaskRepository;
+    }
 
     @PostConstruct
     public void init() {
@@ -32,6 +44,25 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             Long chatId = update.message().chat().id();
             if (update.message().text().equals("/start")) {
                 SendMessage message = new SendMessage(chatId, String.format("Привет, %s! Введи задачу в формтае 01.01.2022 20:00 Сделать домашнюю работу", update.message().from().firstName()));
+                telegramBot.execute(message);
+            }
+
+            Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+            Matcher matcher = pattern.matcher(update.message().text());
+            String date = null;
+            String item = null;
+
+            if (matcher.matches()) {
+                date = matcher.group(1);
+                item = matcher.group(3);
+                logger.info("Date: {}, item: {}", date, item);
+            }
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            if (date != null) {
+                LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+                notificationTaskRepository.save(new NotificationTask(chatId, item, dateTime));
+                SendMessage message = new SendMessage(chatId, "Событие сохранено!");
                 telegramBot.execute(message);
             }
         });
